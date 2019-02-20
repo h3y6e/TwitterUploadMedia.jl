20ffbb6a3c33e683386452318667750fd4dd8715
KAWASE Hiroya <hash.a5ebec@gmail.com>
2019-02-21 04:42:29 +0900

    split testset

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,20 +2,22 @@ using TwitterUploadMedia
 using Test
 using Base64
 
-@testset "TwitterUploadMedia.jl" begin
-    TwitterUploadMedia.twitterauth(ENV["CONSUMER_KEY"],
+TwitterUploadMedia.twitterauth(ENV["CONSUMER_KEY"],
                 ENV["CONSUMER_SECRET"],
                 ENV["ACCESS_TOKEN"],
                 ENV["ACCESS_TOKEN_SECRET"]
     )
-    # POST media/upload
+
+@testset "POST media/upload" begin
     postmediaupload = post_media_upload(media_data=base64encode(open("lena.png")))
     postmedia_id = postmediaupload["media_id"]
     @test typeof(postmeidaupload) == Media
     @test postmediaupload["image_type"] == "image/jpeg"
     @test typeof(postmedia_id) == Int
+end
 
-    # GET media/upload (STATUS)
+@testset "GET media/upload (STATUS)" begin
     getmediaupload = get_media_upload(command="STATUS", media_id=postmedia_id)
+    @test getmediaupload != Nothing
     # WIP
 end
